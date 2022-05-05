c3ba12a8ce3e1c1a30f1897c8734f3875f36b849
swistek <swistekxd@gmail.com>
2022-05-05 03:45:16 +0200

    secound commit

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const icon = document.querySelector('.menu-icon');
 const menuElements = document.querySelectorAll('.navbar__item');
 
-console.log('icon');
 icon.addEventListener('click', () => {
   menuElements.forEach((e) => {
     e.classList.toggle('active');
